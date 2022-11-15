7008536c1adeb83702fa6473ff1cb81b05522175
navneet-Singh13 <109146994+navneet-Singh13@users.noreply.github.com>
2022-11-15 06:27:22 -0800

    project added
    project added

--- script.js
+++ script.js
@@ -16,10 +16,9 @@ change.addEventListener('click' ,  ( ) => {
   }
   
   
-   else{ 
+  else{ 
       
       body.setAttribute("background" , "https://cdn.theatlantic.com/thumbor/i9jH00gdQ3Lov_7DI2xDujaTwQQ=/845x470:2635x1477/1600x900/media/img/mt/2016/03/RTX283V4/original.jpg") ;
-
       change.innerHTML = "DARK" ;
     }
   
